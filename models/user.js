6db74ec26007a26a38d6eeab8c07d0e082468863
abantoo <abantonaimur@gmail.com>
2022-08-10 00:32:38 +0600

    working with cart

--- models/user.js
+++ models/user.js
@@ -2,14 +2,29 @@ const mongodb = require("mongodb");
 const getDB = require("../util/database").getDB;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.username = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
     const db = getDB();
     return db.collection("Users").insertOne(this);
+  }
+
+  addToCart(book) {
+    const updateCart = { items: [{ ...book, quantity: 1 }] };
+    const db = getDB();
+    return db
+      .collection("Users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updateCart } }
+      )
+      .then()
+      .catch();
   }
 
   static findByID(userID) {
