97839343cda0e3b4282dc52360dd5352d79146ec
abantoo <abantonaimur@gmail.com>
2022-08-11 23:43:48 +0600

    added mouseover cringe

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,34 @@
-const backdrop = document.querySelector('.backdrop');
-const sideDrawer = document.querySelector('.mobile-nav');
-const menuToggle = document.querySelector('#side-menu-toggle');
+const backdrop = document.querySelector(".backdrop");
+const sideDrawer = document.querySelector(".mobile-nav");
+const menuToggle = document.querySelector("#side-menu-toggle");
+const card_action = document.querySelector(".card__actions");
+const card_content = document.querySelector(".card__content");
+const card = document.querySelector(".card");
 
 function backdropClickHandler() {
-  backdrop.style.display = 'none';
-  sideDrawer.classList.remove('open');
+  backdrop.style.display = "none";
+  sideDrawer.classList.remove("open");
 }
 
 function menuToggleClickHandler() {
-  backdrop.style.display = 'block';
-  sideDrawer.classList.add('open');
+  backdrop.style.display = "block";
+  sideDrawer.classList.add("open");
 }
 
-backdrop.addEventListener('click', backdropClickHandler);
-menuToggle.addEventListener('click', menuToggleClickHandler);
+function notShowFullCard() {
+  card_action.style.display = "none";
+}
+function showFullCard() {
+  card_action.classList.remove("hidden");
+  card_content.classList.remove("hidden");
+}
+
+function hideFullCard() {
+  card_action.classList.add("hidden");
+  card_content.classList.add("hidden");
+}
+
+backdrop.addEventListener("click", backdropClickHandler);
+menuToggle.addEventListener("click", menuToggleClickHandler);
+card.addEventListener("mouseover", showFullCard);
+card.addEventListener("mouseout", hideFullCard);
