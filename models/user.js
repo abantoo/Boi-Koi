295f6f565d69603e91715bc262b001525bedd8a9
abantoo <abantonaimur@gmail.com>
2022-08-12 22:49:19 +0600

    cart saving in dbs correctly

--- models/user.js
+++ models/user.js
@@ -15,8 +15,23 @@ class User {
   }
 
   addToCart(book) {
+    const cartItemIndex = this.cart.items.findIndex((ci) => {
+      return ci.bookID == book._id;
+    });
+
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartItemIndex >= 0) {
+      newQuantity = this.cart.items[cartItemIndex].quantity + 1;
+      updatedCartItems[cartItemIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push([
+        { bookID: new mongodb.ObjectId(book._id), quantity: newQuantity },
+      ]);
+    }
     const updateCart = {
-      items: [{ bookID: new mongodb.ObjectId(book._id), quantity: 1 }],
+      items: updatedCartItems,
     };
     const db = getDB();
     return db
