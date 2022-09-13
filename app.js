a4b87966b87b2e0d09580583766d7347308c3245
abantoo <abantonaimur@gmail.com>
2022-09-14 00:55:26 +0600

    added login form and route

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
 const User = require("./models/user");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,6 +34,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
